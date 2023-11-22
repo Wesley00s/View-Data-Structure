edbebb7913128b36cc7e6e4303bf568f3d0d41e2
Wesley00s <wesley300rodrigues@gmail.com>
2023-11-21 22:16:16 -0300

    Navbar Corrections

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-let menu = document.querySelector('#menu')
-let navbar = document.querySelect('.navbar')
+// let menu = document.querySelector('#menu')
+// let navbar = document.querySelect('.navbar')
 
-menu.onlick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
+// menu.onlick = () => {
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// }
+
+const menu = document.querySelector('.menuIcon');
+
+menu.addEventListener('click', () => {
+    
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('hiddenMenu');
+});
