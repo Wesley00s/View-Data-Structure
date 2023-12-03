63fd9d8c340e839250a131e71edfdecf49d3e6ae
Wesley00s <wesley300rodrigues@gmail.com>
2023-12-03 20:27:35 -0300

    Dynamic BG List

--- js/jsListas.js
+++ js/jsListas.js
@@ -7,6 +7,13 @@ const valueInput = document.querySelector('.searchMovie')
 const hiddenOpcoes = document.querySelector('.hiddenOpcoes');
 const btnCancelAddMeio = document.querySelector('.cancelAddMeio');
 const btnConfirmAddMeio = document.querySelector('.confirmAddMeio');
+const description = document.querySelector('.desc');
+const titleSelected = document.querySelector('.titleSelected');
+const yearSelected = document.querySelector('.year');
+const bgList = document.querySelector('.bgList');
+const boxDesc = document.querySelector('.boxDesc');
+            const listBox = document.querySelector('.listBox');
+
 
 class AddImagesSlider
 {
@@ -77,15 +84,186 @@ const startSliderAnimation = () =>
 startSliderAnimation();
 
 const movies = [
-    {id : 1, completeTitle : 'Ad Astra: Rumo às Estrelas (2019)', name: 'Ad Astra', pathFile : '../images/imgListas/list/Ad-Astra.png'},
-    {id : 2, completeTitle : 'Akira (1988)', name: 'Akira', pathFile : '../images/imgListas/list/Akira.png'},
-    {id : 3, completeTitle : 'Alien - O Oitavo Passageiro (1979)', name: 'Alien', pathFile : '../images/imgListas/list/Alien.png'},
-    {id : 4, completeTitle : 'Aniquilação (2019)', name: 'Aniquilação', pathFile : '../images/imgListas/list/Aniquilacao.png'},
-    {id : 5, completeTitle : 'A chegada (2016)', name: 'Arrival', pathFile : '../images/imgListas/list/Arrival.png'},
-    {id : 6, completeTitle : '2001: Uma Odisséia no Espaço (1968)', name: '2001: UOE', pathFile : '../images/imgListas/list/ASO.png'},
-    {id : 7, completeTitle : 'A.I.: Inteligência Artificial (2001)', name: 'A.I', pathFile : '../images/imgListas/list/IA.png'},
-    {id : 8, completeTitle : 'A Origem - Inception (2010)', name: 'Inception', pathFile : '../images/imgListas/list/Inception.png'},
-    {id : 9, completeTitle : '13° Andar (1999)', name: '13° Andar', pathFile : '../images/imgListas/list/TTT.png'}
+    {
+        id : 1,
+        bg : '../images/imgListas/list/astra-bg.png',
+        completeTitle: 'Ad Astra: Rumo às Estrelas',
+        year : '(2019)',
+        name : 'Ad Astra',
+        pathFile: '../images/imgListas/list/Ad-Astra.png',
+        desc : `
+                    Este é outro título que você precisa assistir se é grande fã de ficção científica.
+                    Ad Astra coloca Roy McBride(Brad Pitt) em uma viagem ao espaço para encontrar seu pai,
+                    também astronauta.Os problemas começam a surgir quando ele descobre uma ameaça que pode
+                    destruir a Terra e precisa correr contra o tempo para avisar todos em nosso planeta.
+                `
+    },
+    {
+        id: 2, bg: '../images/imgListas/list/akira-bg.png',
+        completeTitle: 'Akira',
+        year : '(1988)',
+        name: 'Akira',
+        pathFile: '../images/imgListas/list/Akira.png',
+        desc : `
+                    Akira é uma animação que engloba ficção científica, ação e drama com direção
+                    e roteiro de Katsuhiro Ôtomo. Uma grande explosão fez com que Tóquio fosse destruída
+                    em 1988. Em seu lugar foi construída Neo Tóquio, que, em 2019, sofre com atentados
+                    terroristas por toda a cidade. Kaneda (Mitsuo Iwata) e Tetsuo (Nozomu Sasaki) são amigos
+                    que integram uma gangue de motoqueiros. Eles disputam rachas violentos com uma gangue
+                    rival, os Palhaços, até que um dia Tetsuo encontra Takashi (Tatsuhiko Nakamura),
+                    uma estranha criança com poderes que fugiu do hospital onde era mantido como cobaia.
+                    Tetsuo é ferido no encontro e antes de receber a ajuda dos amigos é levado por integrantes
+                    do exército, liderados pelo coronel Shikishima (Tarô Ishida). A partir de então Tetsuo
+                    passa a desenvolver poderes inimagináveis, o que faz com que seja comparado ao lendário
+                    Akira, responsável pela explosão de 1988. Paralelamente, Kaneda se interessa por Kei
+                    (Mami Koyama), uma garota envolvida com espiões que tenta decifrar o enigma por
+                    trás das cobaias controladas pelo exército.
+                `
+    },
+    {
+        id : 3,
+        bg : '../images/imgListas/list/alien-bg.png',
+        completeTitle: 'Alien - O Oitavo Passageiro',
+        year : '(1979)',
+        name : 'Alien',
+        pathFile: '../images/imgListas/list/Alien.png',
+        desc : `
+                    Uma nave espacial, ao retornar para Terra, recebe estranhos
+                    sinais vindos de um asteróide. Ao investigarem o local, um
+                    dos tripulantes é atacado por um estranho ser. O que parecia
+                    ser um ataque isolado se transforma em um terror constante, pois
+                    o tripulante atacado levou para dentro da nave o embrião de um
+                    alienígena, que não para de crescer e tem como meta matar toda a tripulação.
+                    Alien é um clássico da ficção científica dirigido por Ridley Scott
+                    que ganhou sequências, crossovers e prequels. Sua cronologia é um
+                    tanto confusa, mas pode ser organizada dessa forma:
+
+                    Prometheus (2012), dirigido por Ridley Scott;
+                    Alien: Covenant (2017), dirigido por Ridley Scott;
+                    Alien, o Oitavo Passageiro (Alien, 1979), dirigido por Ridley Scott;
+                    Aliens, O Resgate (Aliens, 1986), dirigido por James Cameron;
+                    Alien 3 (1992), dirigido por David Fincher;
+                    Alien – A Ressurreição (Alien: Resurrection, 1997), dirigido por Jean-Pierre Jeunet.
+                    Fora dessa linha temporal estão os crossovers com o universo de O Predador.
+                `
+    },
+    {
+        id : 4,
+        bg : '../images/imgListas/list/aniquilacao-bg.png',
+        completeTitle: 'Aniquilação',
+        year : '(2019)',
+        name : 'Aniquilação',
+        pathFile: '../images/imgListas/list/Aniquilacao.png',
+        desc : `
+                    Uma bióloga (Natalie Portman) se junta a uma expedição
+                    secreta com outras três mulheres em uma região conhecida
+                    como Área X, um local isolado da civilização onde as leis
+                    da natureza não se aplicam. Lá, ela precisa lidar com uma
+                    misteriosa contaminação, um animal mortal e ainda procurar
+                    por pistas de colegas que desaparecem, incluindo seu marido
+                    (Oscar Isaac). O longa conta ainda com Jennifer Jason Leigh,
+                    Gina Rodriguez e Tessa Thompson no elenco, com direção de Alex Garland.
+                `
+    },
+    {
+        id : 5,
+        bg : '../images/imgListas/list/arrival-bg.png',
+        completeTitle: 'A chegada',
+        year : '(2016)',
+        name : 'Arrival',
+        pathFile: '../images/imgListas/list/Arrival.png',
+        desc : `
+                    Quando seres interplanetários deixam marcas na Terra,
+                    a Dra. Louise Banks (Amy Adams), uma linguista especialista
+                    no assunto, é procurada por militares para traduzir os sinais
+                    e desvendar se os alienígenas representam uma ameaça ou não.
+                    No entanto, a resposta para todas as perguntas e mistérios
+                    pode ameaçar a vida de Louise e a existência de toda a humanidade.
+                    O elenco do longa conta ainda com nomes como Jeremy Renner e Forest
+                    Whitaker, e a direção fica por conta de Denis Villeneuve.
+                `
+    },
+    {
+        id : 6,
+        bg : '../images/imgListas/list/ASO-bg.png',
+        completeTitle : '2001: Uma Odisséia no Espaço',
+        year : '(1968)',
+        name : '2001: UOE',
+        pathFile: '../images/imgListas/list/ASO.png',
+        desc : `
+                    "2001" é uma história de evolução. Em algum momento no passado distante,
+                    alguém ou algo incentivou a evolução ao colocar um monólito na Terra
+                    (presumivelmente também em outro lugar do universo). A evolução permitiu
+                    então que a humanidade alcançasse a superfície da Lua, onde foi encontrado
+                    outro monólito, um que sinaliza aos placers de monólitos que a humanidade
+                    evoluiu até aqui. Agora a humanidade, auxiliada pela inteligência artificial
+                    (como HAL), deve ousar encontrar os colocadores de monólitos. Se for bem sucedido,
+                    o próximo passo na evolução – seja ele qual for – será alcançado.—Larry Cousins
+
+                `
+    },
+    {
+        id : 7,
+        bg : '../images/imgListas/list/AI-bg.png',
+        completeTitle : 'A.I.: Inteligência Artificial',
+        year : '(2001)',
+        name: 'A.I',
+        pathFile: '../images/imgListas/list/IA.png',
+        desc: `
+                Na metade do século XXI, o efeito estufa derreteu uma grande parte das calotas
+                polares da Terra, fazendo com que boa parte das cidades litorâneas do planeta
+                fiquem parcialmente submersas. Para controlar este desastre ambiental a humanidade
+                conta com o auxílio de uma nova forma de computador independente, com inteligência
+                artificial, conhecido como A.I. É neste contexto que vive o garoto David Swinton
+                (Haley Joel Osment), que irá passar por uma jornada emocional inesquecível.
+                O longa dramático tem direção de Steven Spielberg e roteiro assinado por Spielberg
+                e Ian Watson. O elenco composto ainda por Jude Law, Frances O'Connor e Sam Robards.
+            `
+    },
+    {
+        id : 8,
+        bg : '../images/imgListas/list/inception-bg.png',
+        completeTitle : 'A Origem - Inception',
+        year : '(2010)',
+        name: 'Inception',
+        pathFile: '../images/imgListas/list/Inception.png',
+        desc : `
+                    Com direção e roteiro de Christopher Nolan, A Origem mistura
+                    suspense e ficção científica com uma pitada de ação em um longa
+                    que não é para qualquer um. Em um mundo onde é possível entrar na
+                    mente humana, Cobb (Leonardo DiCaprio) está entre os melhores na
+                    arte de roubar segredos valiosos do inconsciente, durante o estado
+                    de sono. Além disto ele é um fugitivo, pois está impedido de retornar
+                    aos Estados Unidos devido à morte de Mal (Marion Cotillard). Desesperado
+                    para rever seus filhos, Cobb aceita a ousada missão proposta por Saito
+                    (Ken Watanabe), um empresário japonês: entrar na mente de Richard Fischer
+                    (Cillian Murphy), o herdeiro de um império econômico, e plantar a ideia
+                    de desmembrá-lo. Para realizar este feito ele conta com a ajuda do parceiro
+                    Arthur (Joseph Gordon-Levitt), a inexperiente arquiteta de sonhos Ariadne
+                    (Elliot Page) e Eames (Tom Hardy), que consegue se disfarçar de forma
+                    precisa no mundo dos sonhos.
+                `
+    },
+    {
+        id : 9,
+        bg : '../images/imgListas/list/TTT-bg .png',
+        completeTitle : '13° Andar',
+        year : '(1999)',
+        name: '13° Andar',
+        pathFile: '../images/imgListas/list/TTT.png',
+        desc: `
+                    O cientista da computação Hannon Fuller descobriu algo extremamente
+                    importante. Ele está prestes a contar a descoberta ao seu colega
+                    Douglas Hall, mas sabendo que alguém está atrás dele, o velho deixa
+                    uma carta no mundo paralelo gerado por computador que sua empresa criou
+                    (que parece os anos 30 com pessoas aparentemente reais com emoções reais).
+                    Fuller é assassinado em nosso mundo real na mesma noite, e seu colega é suspeito.
+                    Douglas descobre uma camisa ensanguentada em seu banheiro e não consegue se
+                    lembrar do que estava fazendo na noite em que Fuller foi assassinado. Ele faz
+                    login no sistema para encontrar a carta, mas precisa enfrentar o inesperado.
+                    A verdade é mais dura do que ele jamais poderia imaginar...—Danny Rosenbluth
+                `
+    }
 ]
 
 const containerAvailable = document.querySelector('.containerAvailable');
@@ -197,6 +375,7 @@ const createDynamicElement = (numb, imagePath) => {
 
     const imgElement = document.createElement('img');
     imgElement.className = 'itemList';
+    imgElement.classList.add('classUserList');
     imgElement.src = imagePath;
     imgElement.alt = 'img list';
 
@@ -210,18 +389,13 @@ const createDynamicElement = (numb, imagePath) => {
 
     document.querySelector('.listBox').appendChild(divElement);
 
-    // console.log('createDynamicElement - Index:', numb);
-    // console.log('createDynamicElement - PathFile:', imagePath);
     return divElement;
 }
-
-// userList.forEach(node => {
-//     createDynamicElement(node);
-// });
 
 const itemBox = document.querySelector('.itemBox');
 const imgItemBoxElement = document.createElement('img');
 const spanItemBoxElement = document.createElement('span');
+
 
 let index;
 const imgAddedName = [];
@@ -289,37 +463,37 @@ const handleButtonClick = (actionType) =>
                     btnAddInicio.classList.toggle('hiddenInput');
                     btnAddFim.classList.toggle('hiddenInput');
                     btnAddMeio.classList.toggle('hiddenInput');
+                    return;
 
                     console.log(`Input: ${valueInput.value}`);
-                    if (valueInput.value !== '') {
-                        btnConfirmAddMeio.addEventListener('click', () => {
-                            element = createDynamicElement(index + 1, valueInput[index].pathFile);
-                            userList.insertAfter(parseInt(valueInput.value), element);
-                            insertMethod = 'insertMiddle';
-                            console.log('AddMeioClicked!');
-                        });
+                //     if (valueInput.value !== '') {
+                //         btnConfirmAddMeio.addEventListener('click', () => {
+                //             element = createDynamicElement(index + 1, valueInput[index].pathFile);
+                //             userList.insertAfter(parseInt(valueInput.value), element);
+                //             insertMethod = 'insertMiddle';
+                //             console.log('AddMeioClicked!');
+                //         });
 
-                        btnCancelAddMeio.addEventListener('click', () => {
-                        input.classList.toggle('hiddenInput');
-                        btnAddInicio.classList.toggle('hiddenInput');
-                        btnAddFim.classList.toggle('hiddenInput');
-                        btnAddMeio.classList.toggle('hiddenInput');
-                    });
-                }
+                //         btnCancelAddMeio.addEventListener('click', () => {
+                //         input.classList.toggle('hiddenInput');
+                //         btnAddInicio.classList.toggle('hiddenInput');
+                //         btnAddFim.classList.toggle('hiddenInput');
+                //         btnAddMeio.classList.toggle('hiddenInput');
+                //     });
+                // }
                 break;
             default:
                 console.error('Ação não reconhecida.');
                 return;
             }
 
-            const listBox = document.querySelector('.listBox');
             listBox.innerHTML = '';
 
             userList.forEach((node) => {
                 listBox.appendChild(node.data);
             });
 
-            document.querySelector('.listBox').addEventListener('click', (event) => {
+            listBox.addEventListener('click', (event) => {
                 if (event.target.classList.contains('btnRemove')) {
                     const itemBox = event.target.closest('.itemBox');
 
@@ -371,13 +545,43 @@ const handleButtonClick = (actionType) =>
 }
 
 
-btnCancelAddMeio.addEventListener('click', () => { 
-    input.classList.toggle('hiddenInput');
-    btnAddInicio.classList.toggle('hiddenInput');
-    btnAddFim.classList.toggle('hiddenInput');
-    btnAddMeio.classList.toggle('hiddenInput');
+ listBox.addEventListener('click', (event) => { 
+    if (event.target.classList.contains('classUserList')) 
+    {
+        const itemBox = event.target.closest('.classUserList');
+        
+        for (let i = 0; i < movies.length; i++)
+        {
+            let url = movies[i].pathFile;
+            let srcSelected = new URL(url, window.location.origin).href;
+            if (srcSelected === itemBox.src)
+            {
+                src = movies[i].bg;
+                titleSelected.innerHTML = '';
+                description.innerHTML = '';
+
+                titleSelected.innerHTML = movies[i].completeTitle;
+                
+                description.innerHTML = movies[i].desc
+                yearSelected.innerHTML = movies[i].year;
+                // console.log(url)
+                console.log(bgList);
+                bgList.src = src;  
+                console.log("Elemento clicado!");
+                bgList.classList.toggle('bgListAnimation');
+                boxDesc.classList.toggle('animationBoxDesc');
+            }
+        }
+        
+    }
 });
 
+// btnCancelAddMeio.addEventListener('click', () => { 
+//     input.classList.toggle('hiddenInput');
+//     btnAddInicio.classList.toggle('hiddenInput');
+//     btnAddFim.classList.toggle('hiddenInput');
+//     btnAddMeio.classList.toggle('hiddenInput');
+// });
 
 btnAddInicio.addEventListener('click', () => handleButtonClick('addInicio'));
 btnAddFim.addEventListener('click', () => handleButtonClick('addFim'));
