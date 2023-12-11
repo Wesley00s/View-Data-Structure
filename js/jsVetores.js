321d010f934d0ce36195b5e7f29215d549aa5928
Wesley00s <wesley300rodrigues@gmail.com>
2023-12-11 18:06:22 -0300

    Arrays

--- js/jsVetores.js
+++ js/jsVetores.js
@@ -8,10 +8,12 @@ const yearSelected = document.querySelector('.year');
 const bgList = document.querySelector('.bgList');
 const boxDesc = document.querySelector('.boxDesc');
 const listBox = document.querySelector('.listBox');
+const listBox2 = document.querySelector('.listBox2');
 const btnFechar = document.querySelector('.btnFechar');
 const menuTitulos = document.querySelector('.menuTitulos');
 const titleOpc = document.querySelector('.titleOpc');
-
+const listBoxUser = document.querySelector('.listBoxUser');
+const itemList = document.querySelector('.itemList');
 
 class AddImagesSlider{
     constructor(imgArray, imgName){
@@ -25,16 +27,15 @@ class AddImagesSlider{
     }
     createImageElement(){
         this.imgArray.forEach(imgSrc => 
-            {
-                const img = document.createElement('img');
-                img.src = imgSrc;
-                img.classList.add('maxWidthSliders');
-                img.classList.add('slider');
-                imgsContainer.appendChild(img);
-            });
+        {
+            const img = document.createElement('img');
+            img.src = imgSrc;
+            img.classList.add('maxWidthSliders');
+            img.classList.add('slider');
+            imgsContainer.appendChild(img);
+        });
     }
 };
-
 
 const images = [];
 
@@ -61,39 +62,7 @@ const games = [
 ]
 
 const containerAvailable = document.querySelector('.containerAvailable');
-/*
-games.forEach((game) => {
-    const itemBox = document.createElement('div');
-    itemBox.classList.add('itemBox');
 
-    const img = document.createElement('img');
-    img.classList.add('itemList', 'listAvailable');
-    img.src = game.imagem;
-    img.alt = 'img list';
-
-    const span = document.createElement('span');
-    span.classList.add('nome');
-    span.textContent = game.nome;
-
-    // Adiciona o botão "Adicionar"
-    const addButton = document.createElement('button');
-    addButton.classList.add('btnAdicionar');  // Adicione a classe que desejar ao botão
-    addButton.textContent = 'Adicionar';
-
-    // Adiciona os elementos ao itemBox
-    itemBox.appendChild(img);
-    itemBox.appendChild(span);
-    itemBox.appendChild(addButton);
-
-    containerAvailable.appendChild(itemBox);
-
-    addButton.addEventListener('click', () => {
-        // Adicione aqui o código para lidar com o clique no botão "Adicionar"
-        // Você pode usar a variável game para obter as informações do jogo correspondente
-        console.log('Botão Adicionar clicado para o jogo:', game.nome);
-    });
-});
-*/
 games.forEach((game) => {
     const itemBox = document.createElement('div');
     itemBox.classList.add('itemBox');
@@ -126,43 +95,16 @@ games.forEach((game) => {
         const listBoxItem = createDynamicElement(game.imagem);
         // Adiciona as informações do jogo ao elemento criado
         listBoxItem.querySelector('.itemList').src = game.imagem;
-        listBoxItem.querySelector('.nome').textContent = game.nome;
-
+        listBoxItem.querySelector('.name').textContent = game.nome;
+    
         // Adicione aqui a lógica para lidar com o clique no botão "Adicionar"
         // Você pode usar a variável game para obter as informações do jogo correspondente
         console.log('Botão Adicionar clicado para o jogo:', game.nome);
-
+    
         // Adiciona o elemento criado à listBox
-        listBox.appendChild(listBoxItem);
+        itemList.appendChild(listBoxItem);
     });
 });
-
-
-/*
-// Itera sobre cada filme na lista 'movies'
-games.forEach((game) => {
-    
-    const itemBox = document.createElement('div');
-    itemBox.classList.add('itemBox'); 
-    itemBox.style.flexDirection = 'column'; // Adiciona a propriedade CSS para organizar em coluna
-
-    const img = document.createElement('img');
-    img.classList.add('itemList', 'listAvailable'); 
-    img.src = game.imagem; 
-    img.alt = 'img list'; 
-    // Cria um elemento span para exibir o nome do filme
-    const span = document.createElement('span');
-    span.classList.add('name'); // Adiciona a classe 'name' ao elemento span
-    span.textContent = game.nome; // Define o texto do span com base na propriedade 'name' do filme
-
-    // Adiciona a imagem e o span ao elemento div 'itemBox'
-    itemBox.appendChild(img);
-    itemBox.appendChild(span);
-
-    // Adiciona o elemento 'itemBox' ao elemento com a classe 'containerAvailable'
-    containerAvailable.appendChild(itemBox);
-});
-*/
 
 const showAvailableGames = () => {
     containerAvailable.innerHTML = '';
@@ -187,7 +129,6 @@ const showAvailableGames = () => {
     });
 };
 
-
 const itemBox = document.querySelector('.itemBox');
 const imgItemBoxElement = document.createElement('img');
 const spanItemBoxElement = document.createElement('span');
@@ -209,25 +150,17 @@ const createDynamicElement = (imagePath) => {
     imgElement.src = imagePath;
     imgElement.alt = 'img list';
 
-    const btnRemoveElement = document.createElement('button');
-    btnRemoveElement.className = 'btnRemove';
-    btnRemoveElement.textContent = '-';
+    // const btnRemoveElement = document.createElement('button');
+    // btnRemoveElement.className = 'btnRemove';
+    // btnRemoveElement.textContent = '-';
 
     divElement.appendChild(spanElement);
     divElement.appendChild(imgElement);
-    divElement.appendChild(btnRemoveElement);
+    // divElement.appendChild(btnRemoveElement);
 
-    listBox.appendChild(divElement);
+    listBox2.appendChild(divElement);
 
     return divElement;
-}
-
-const resetInputVisibility = () =>
-{
-    input.classList.add('hiddenInput');
-    btnAddInicio.classList.remove('hiddenInput');
-    btnAddFim.classList.remove('hiddenInput');
-    btnAddMeio.classList.remove('hiddenInput');
 }
 
 // Função para lidar com o clique em botões
@@ -320,61 +253,13 @@ itemBox.addEventListener('click', (event) =>
             selectedElement = itemBox;
         }
  
-        // This Section is for Github Pages***********************************************
-        for (let i = 0; i < games.length; i++)
-        {
-            let url = games[i].imagem;
-            let itemBoxSrc = itemBox.src
-            let relativeItemBox = new URL(itemBoxSrc, window.location.origin).pathname
-            let relativeUrl = new URL(url, window.location.origin).pathname
-
-            let convertURL = `/View-Data-Structure${relativeUrl.toString()}`;
-            let convertItemBox = relativeItemBox.toString();
-            console.log(`itemBox.src: ${itemBoxSrc}`);
-            console.log(`url: ${url}`);
-            console.log(`RelativeItemBox: ${relativeItemBox}`)
-            console.log(`Relative url: ${relativeUrl}`);
-            console.log(`convertURL: ${convertURL}`);
-            console.log(`convertItemBox ${convertItemBox}`);
-            if (convertURL == convertItemBox)
-            {
-                titleSelected.innerHTML = '';
-                description.innerHTML = '';
-                src = movies[i].bg;
-
-                titleSelected.innerHTML = games[i].completeTitle;
-                
-                description.innerHTML = games[i].desc
-                yearSelected.innerHTML = games[i].year;
-                // console.log(bgList);
-                bgList.src = src;  
-                console.log("Elemento clicado!");
-                bgList.classList.toggle('bgListAnimation');
-                boxDesc.classList.toggle('animationBoxDesc');
-            }
-        }
-        // This Section is for Github Pages***********************************************
     }
 });
 
-
 const handleClick = () =>
 {
-    // Alterna entre as classes 'hiddenSideAvailable' e 'visibleSideAvailable' no menu de títulos
     menuTitulos.classList.toggle('hiddenSideAvailable');
     menuTitulos.classList.toggle('visibleSideAvailable');
 
-    
-    if (menuTitulos.classList.contains('hiddenSideAvailable')) {
-        hiddenOpcoes.classList.add('backAllTranslateHiddenOpcoes');
-    }
-     else {
-        hiddenOpcoes.classList.remove('backAllTranslateHiddenOpcoes');
-    }
-
-    // Alterna entre as classes 'translateHiddenOpcoes1' e 'translateHiddenOpcoes2' no menu de opções escondido
-    hiddenOpcoes.classList.toggle('translateHiddenOpcoes1');
-    hiddenOpcoes.classList.toggle('translateHiddenOpcoes2');
 };
 document.querySelector('.botaoVerGames').addEventListener('click', handleClick);
-document.querySelector('.arrowImg').addEventListener('click', handleClick);
